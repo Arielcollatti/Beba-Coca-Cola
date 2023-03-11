8df93250839a38769def79f5bdab3b994e51579b
arielcollatti <colatti2017@gmail.com>
2023-03-10 22:39:23 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("beba coca cola ");
+Console.WriteLine();
+
+Console.WriteLine("beba coca cola ");
+Console.WriteLine("beba      cola ");
+Console.WriteLine("beba coca ");
+Console.WriteLine("beba cola caco ");
+Console.WriteLine("caco ");
+Console.WriteLine("cola ");
+Console.WriteLine("     cloaca ");
